eb8e457bab3898257949f52100854a7603ab4e1d
Robert Gregg <hi@robertgregg.co.uk>
2022-03-14 10:11:42 +0000

    Added TM symbol

--- src/components/dataVisualisation/DataVisualisation.jsx
+++ src/components/dataVisualisation/DataVisualisation.jsx
@@ -354,7 +354,7 @@ export const DataVisualisation = (rawData) => {
         <div className="logo__and__copyright">
           <img alt="Thebes logo" className="thebes-logo" src={logo} height="100px" />
         <span>Copyright © 2022. All Rights Reserved.</span> <br />
-        <span>ATAITM & ATAI Wave® are registered trademarks owned by Thebes Cloud Management Ltd.</span>
+        <span>ATAI™ & ATAI Wave® are registered trademarks owned by Thebes Cloud Management Ltd.</span>
         </div>
       </div>
 
