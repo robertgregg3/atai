2e85f283df8f60e6f3b330856ebe00970bc14bb2
Robert Gregg <hi@robertgregg.co.uk>
2024-11-11 14:25:43 +0000

    feat: update chart with download button

--- src/components/charts/SavingsTotalsBarChart.jsx
+++ src/components/charts/SavingsTotalsBarChart.jsx
@@ -1,82 +1,83 @@
 import { useEffect } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import './savingsTotalsBarChart.css'
+import "./savingsTotalsBarChart.css";
+import DownloadChart from "../../utils/downloadChart";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const SavingsTotalsBarChart = ({ chartData, chartTitle }) => {
+const SavingsTotalsBarChart = ({ chartData, exportSavingsTotals }) => {
   useEffect(() => {}, []);
-
 
   const options = {
     responsive: true,
     maintainAspectRatio: false,
     plugins: {
       title: {
-        display: true,
-        text: chartTitle,
+        display: false,
       },
       legend: {
-        position: 'top',
+        position: "top",
         labels: {
           usePointStyle: true,
         },
       },
       tooltip: {
         callbacks: {
-            label: function(context) {
-                let label = context.dataset.label || '';
+          label: function (context) {
+            let label = context.dataset.label || "";
 
-                if (label) {
-                    label += ': ';
-                }
-                if (context.parsed.y !== null) {
-                    label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
-                }
-                return label;
+            if (label) {
+              label += ": ";
             }
-        }
-      } 
+            if (context.parsed.y !== null) {
+              label += new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(context.parsed.y);
+            }
+            return label;
+          },
+        },
+      },
     },
     scales: {
       yAxes: {
         ticks: {
-        callback: function(value) {
-            value = value.toString()
-                         .split(/(?=(?:...)*$)/)
-                         .join(',');
-            return '$' + value;
-          }
-        }
-      }
-    }
+          callback: function (value) {
+            value = value
+              .toString()
+              .split(/(?=(?:...)*$)/)
+              .join(",");
+            return "$" + value;
+          },
+        },
+      },
+    },
   };
 
   const data = {
-    labels: ['Savings Totals'],
+    labels: ["Savings Totals"],
     datasets: [
       {
-        label: 'Current Year Savings',
+        label: "Current Year Savings",
         data: [chartData[0]],
-        backgroundColor: [
-          "#10a8a9",
-        ]},
+        backgroundColor: ["#10a8a9"],
+      },
       {
-        label: 'Yearly Savings',
+        label: "Yearly Savings",
         data: [chartData[1]],
-        backgroundColor: [
-          "#000038"
-        ]},
+        backgroundColor: ["#000038"],
+      },
       {
-        label: 'Monthly Savings',
+        label: "Monthly Savings",
         data: [chartData[2]],
-        backgroundColor: [
-          "#cccccc",
-      ]},
+        backgroundColor: ["#cccccc"],
+      },
     ],
   };
   return (
     <div className="chart-horizontal">
+      <DownloadChart reference={exportSavingsTotals} title={"Savings totals"} />
       <Bar data={data} options={options} />
     </div>
   );
