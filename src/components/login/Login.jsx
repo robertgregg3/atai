69dfad10c7e720d1ea5d6fc095ac286676a9373a
Robert Gregg <hi@robertgregg.co.uk>
2024-11-11 12:36:07 +0000

    feat: set active menu link with styles

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,114 +1,126 @@
-import { useState } from 'react';
-import { auth } from '../../firebaseConfig';
-import { useStateValue } from '../../Context/StateProvider';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import logo from '../../images/thebes.png';
-import ataiLogo from '../../images/atai2.svg';
-import './login.css';
+import { useState } from "react";
+import { auth } from "../../firebaseConfig";
+import { useStateValue } from "../../Context/StateProvider";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import ataiLogo from "../../images/atai1.svg";
+import "./login.css";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
-    const [login, setLogin] = useState(true);
-    const [{user}, dispatch] = useStateValue();
-    const history = useHistory();
-    
-    const handleSignIn = (e) => {
-        e.preventDefault();
-        auth.signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            let loggedInUser = userCredential;
-            console.log('user: ', loggedInUser)
-            dispatch({
-                type: 'SET_USER',
-                user: loggedInUser
-            })
-        })
-    }
-    
-    const handleRegister = (e) => {
-        e.preventDefault();
-        auth
-        .createUserWithEmailAndPassword(email, password)
-        .then((auth) => {
-            console.log('user: ', auth)
-            auth.user.updateProfile({
-                displayName: name,
-            })
-            dispatch({
-                type: 'SET_USER',
-                user: auth.user,
-                displayName: name
-            })
-        })
-        .catch(err => console.log(err))
-        history.push('/');
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [activeButton, setActiveButton] = useState("Login");
+  const [name, setName] = useState("");
+  const [login, setLogin] = useState(true);
+  const [{ user }, dispatch] = useStateValue();
+  const history = useHistory();
 
-    const handleButtonClick = (value) => {
-        setLogin(value)
-      }
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+      let loggedInUser = userCredential;
+      console.log("user: ", loggedInUser);
+      dispatch({
+        type: "SET_USER",
+        user: loggedInUser,
+      });
+    });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log("user: ", auth);
+        auth.user.updateProfile({
+          displayName: name,
+        });
+        dispatch({
+          type: "SET_USER",
+          user: auth.user,
+          displayName: name,
+        });
+      })
+      .catch((err) => console.log(err));
+    history.push("/dashboard");
+  };
+
+  const handleButtonClick = (value, linkName) => {
+    setActiveButton(linkName);
+    setLogin(value);
+  };
 
   return (
-      <>
-          <div className="login__container">
-            <div className="login__left-side">
-               <img alt="Thebes logo" src={logo} /> 
-            </div>
-            <div>
-                <div className="login__options">
-                    <button onClick={() => handleButtonClick(true)}>Login</button>
-                    <button onClick={() => handleButtonClick(false)}>Create Account (DEMO Only)</button>
-                </div>
-                <div className="login__container-header">
-                    <img alt="Atai Logo" src={ataiLogo} />
-                </div>
-                <form>
-                    {!login && (
-                        <>
-                            <label>Full Name</label>
-                            <input
-                                type="text"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </>
-                        )}
-                    <label>E-mail</label>
-                    <input
-                        type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    
-                    <div className="login__button-container">
-                        {login 
-                            ? (<button
-                                type="submit"
-                                onClick={handleSignIn}
-                                >
-                                Sign in
-                            </button>)
-                            : (
-                                <button 
-                                onClick={handleRegister} 
-                                className="login__registerAccount">
-                                Create ATAI Account
-                                </button>
-                            )}
-                    </div>
-                </form>
-            </div>
+    <>
+      <div className="login__container">
+        <div className="login__left-side">
+          <div className="login__container-header">
+            <img
+              alt="Atainr Logo"
+              src={ataiLogo}
+              width={"200px"}
+              height={"200px"}
+            />
           </div>
-      </>
-  )
-}
+        </div>
+        <div>
+          <div className="login__options">
+            <button
+              className={`${activeButton === "Login" ? "login__active" : ""}`}
+              onClick={() => handleButtonClick(true, "Login")}>
+              Login
+            </button>
+            <button
+              className={`${
+                activeButton === "Create Account" ? "login__active" : ""
+              }`}
+              onClick={() => handleButtonClick(false, "Create Account")}>
+              Create Account (DEMO Only)
+            </button>
+          </div>
 
-export default Login
+          <form>
+            {!login && (
+              <>
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </>
+            )}
+            <label>E-mail</label>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className="login__button-container">
+              {login ? (
+                <button type="submit" onClick={handleSignIn}>
+                  Sign in
+                </button>
+              ) : (
+                <button
+                  onClick={handleRegister}
+                  className="login__registerAccount">
+                  Create ATAI Account
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
