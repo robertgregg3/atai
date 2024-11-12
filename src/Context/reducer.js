92e40570c2ad38e8989e363669793fb52641b2ed
Robert Gregg <hi@robertgregg.co.uk>
2024-11-12 16:03:51 +0000

    feat: add sidebar toggle action to reducer

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -1,25 +1,32 @@
 export const initialState = {
-    user: null,
-    data: [],
-    displayName: ''
-}
+  user: null,
+  data: [],
+  displayName: "",
+  sidebarOpen: true,
+};
 
 const reducer = (state, action) => {
-    switch(action.type){
-        case "SET_USER":
-            return {
-                ...state,
-                user: action.user,
-                displayName: action.displayName
-            };
-            case "SET_DATA":
-                return {
-                    ...state,
-                    data: action.data
-                }
-                default:
-                    return state;
-    }
+  console.log("state", state);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+        displayName: action.displayName,
+      };
+    case "SET_DATA":
+      return {
+        ...state,
+        data: action.data,
+      };
+    default:
+      return state;
+    case "TOGGLE_SIDEBAR":
+      return {
+        ...state,
+        sidebarOpen: action.sidebarOpen,
+      };
+  }
 };
 
 export default reducer;
