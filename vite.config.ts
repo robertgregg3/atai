05cb677b5bca5bab171dc744a34058b1c5fe5686
Robert Gregg <hi@robertgregg.co.uk>
2024-11-24 12:31:10 +0000

    use absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   plugins: [react(), dsv()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@images': path.resolve(__dirname, 'src/images'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@context': path.resolve(__dirname, 'src/context'),
-      '@charts': path.resolve(__dirname, 'src/components/charts'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@images': path.resolve(__dirname, './src/images'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@charts': path.resolve(__dirname, './src/components/charts'),
     },
   },
 })
