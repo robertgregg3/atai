08792aef7dd2da03bedc53567e51ca322b909c13
Robert Gregg <hi@robertgregg.co.uk>
2024-11-30 15:55:16 +0000

    Abstract logic to initialise the app with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,30 @@
-import {
-  Navigate,
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { StateContext } from "./context/StateProvider";
 import { useContext, useEffect, useState } from "react";
-import { auth } from "./firebaseConfig";
-import { DataVisualisation } from "./components/dataVisualisation/DataVisualisation";
-import { stateEnums } from "./context/reducer";
 import csvData from "./data/static-data.csv?raw";
-import Login from "./components/login/Login";
-import firebase from 'firebase/compat/app';
 import Papa from "papaparse";
 import Spinner from "@components/ui/Spinner/Spinner";
 import createAppRouter from "./config/appRouter";
+import useInitializeApp from "@hooks/useInitializeApp";
 
 const App = () => {
-  const { state, dispatch} = useContext(StateContext);
+  const { state } = useContext(StateContext);
+  const [ rawData, setRawData ] = useState<any>();
   const { user } = state;
-  const [loading, setLoading] = useState(true); 
-  const [appData, setAppData] = useState<any>();
+  const { appData, isLoading } = useInitializeApp(rawData);
 
   useEffect(() => {
-    const rawData = Papa.parse(csvData, {
+    setRawData(Papa.parse(csvData, {
       header: true, // Set to true if your CSV has headers
       skipEmptyLines: true,
-    }).data;
-    auth.onAuthStateChanged((authUser: firebase.User | null) => {
-      if (authUser) {
-        dispatch({
-          type: stateEnums.SET_USER,
-          payload: {
-            user: authUser,
-            displayName: authUser.displayName || "Unknown"
-          },
-        });
-        dispatch({
-          type: stateEnums.SET_DATA,
-          payload: rawData,
-        });
-        setAppData(rawData)
-      }
-      setLoading(false);
-    });
-  }, [dispatch, csvData]);
-
-  if (loading) {
+    }).data);
+  }, [csvData]);
+ 
+  if (isLoading) {
     return <Spinner />;
   }
   
-  return <RouterProvider router={createAppRouter({isAuthUser: user, appData})} future={{
+  return <RouterProvider router={createAppRouter({isAuthUser: user, appData: appData ?? []})} future={{
     v7_startTransition: true}} />;
 };
 
