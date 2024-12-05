e146171fe615afb29cceeec1b773e2014657d7ba
Robert Gregg <hi@robertgregg.co.uk>
2024-12-05 15:55:09 +0000

    feat: extended button component with icon position

--- src/components/ui/buttons/Button/Button.tsx
+++ src/components/ui/buttons/Button/Button.tsx
@@ -6,25 +6,35 @@ interface ButtonProps {
     text: string;
     className?: string;
     textCenter?: boolean;
+    iconOnRight?: boolean;
 }
 
-const Button = ({ handleClick, icon, text, className, textCenter} : ButtonProps) => {
+const Button = ({ 
+    handleClick, 
+    icon = null, 
+    text, 
+    className = '', 
+    textCenter = false, 
+    iconOnRight = false}: ButtonProps) => {
+    
     const getClassNames = () => {
-        return `
-            ${textCenter ? 'button--text-center' : ''} 
-            ${icon ? 'button__with-icon' : ''} 
-            ${className ? className : ''} 
-        `;
+        const centerText = textCenter ? 'button--text-center' : '';
+        const withIcon = icon ? 'button__with-icon' : '';
+        const iconRight = iconOnRight ? 'icon-on-right' : '';
+        const customClassName = className ? className : '';
+
+        return `${centerText} ${withIcon} ${iconRight} ${customClassName}`;
     }
-  return ( 
-    <button 
-        onClick={handleClick} 
-        className={getClassNames()}
-    >
-        {icon && icon}
-        {icon ? <span>{text}</span> : text}
-    </button>
-  )
+
+    return ( 
+        <button 
+            onClick={handleClick} 
+            className={getClassNames()}
+            aria-label={text}
+        >
+            {icon && icon} {text}
+        </button>
+    )
 }
 
 export default Button
