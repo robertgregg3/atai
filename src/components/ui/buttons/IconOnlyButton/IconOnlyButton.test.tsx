f764d05e55f56ea7cfe852ed7546e93885868b8d
Robert <iamrobertgregg@gmail.com>
2024-12-30 19:07:04 +0000

    test: icon only button

---
+++ src/components/ui/buttons/IconOnlyButton/IconOnlyButton.test.tsx
@@ -0,0 +1,27 @@
+import { describe, expect, it, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { RiMenuFold3Fill } from "react-icons/ri";
+import IconOnlyButton from "./IconOnlyButton";
+
+describe('IconOnlyButton', () => {
+    it('renders the button', () => {
+        const ariaLabel = 'some-label'
+        const handleButtonClick = vi.fn();
+
+        render(
+            <IconOnlyButton
+                ariaLabel={ariaLabel}
+                handleClick={handleButtonClick}
+                icon={<RiMenuFold3Fill />}
+            />
+        );
+
+        const button = screen.getByLabelText(ariaLabel);
+
+        expect(button).toBeInTheDocument();
+
+        fireEvent.click(button);
+
+        expect(handleButtonClick).toHaveBeenCalled();
+    })
+})
