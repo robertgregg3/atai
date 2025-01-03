3bc18e92bda1a2c756cb9927fed720851566092d
Robert <iamrobertgregg@gmail.com>
2025-01-03 13:09:26 +0000

    chore: remove unused file

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,3 +1,3 @@
-export { default as SavingsTotalsBarChart } from './SavingsTotalsBarChart';
 export { default as DoughnutChart } from './DoughnutChart';
 export { default as Chart } from './Chart';
+export * from './chart.types'
