32b3a3278759ab4147d906b83d71927413a36879
Robert <iamrobertgregg@gmail.com>
2025-01-03 13:39:07 +0000

    feat: constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+import { ChartTitlesType } from "@components/charts";
+
+export const chartTitles: Record<ChartTitlesType, string> = {
+  "savings": "Total Savings Bar Chart",
+  "cost": "Cost Savings Bar Chart",
+  "environment": "Environment Savings Bar Chart",
+  "product": "Product Savings Doughnut Chart",
+};
