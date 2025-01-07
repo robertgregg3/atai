a49e108a5016879dce28e2bd08f000c8adca0ac6
Robert <iamrobertgregg@gmail.com>
2025-01-07 14:42:29 +0000

    fix: loading check fix

--- src/context/StateProvider.tsx
+++ src/context/StateProvider.tsx
@@ -45,6 +45,8 @@ export const StateProvider = ({ children  }: { children: React.ReactNode }) => {
                         displayName: "",
                     },
                  });
+                 isAuthChecked = true;
+                 checkLoadingComplete();
             }
         });
     
