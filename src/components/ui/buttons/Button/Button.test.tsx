0feba8fe3b5693c6ecd369139809851dcc68304b
Robert <iamrobertgregg@gmail.com>
2025-01-07 10:06:59 +0000

    build: fix failed test for slack webhook test

--- src/components/ui/buttons/Button/Button.test.tsx
+++ src/components/ui/buttons/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect, vi, afterEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Button from './Button';
+import { Button } from '@components';
 import '@testing-library/jest-dom'; 
 
 const buttonDefaultProps = {
