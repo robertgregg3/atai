de26c95c41581317430414c53e8a8c014129f6b7
Robert <iamrobertgregg@gmail.com>
2025-01-14 12:34:50 +0000

    feat: rename app router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { StateContext } from "./context/StateProvider";
 import { useContext } from "react";
 import Spinner from "@components/ui/Spinner/Spinner";
-import createAppRouter from "./config/appRouter";
+import createAppRouter from "./config/createAppRouter";
 
 const App = () => {
   const { user, isLoading } = useContext(StateContext).state;
