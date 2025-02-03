1190b1440a6a80b065306cea8ab715adc588fca8
Robert <iamrobertgregg@gmail.com>
2025-02-03 12:59:27 +0000

    test: basic test for toast mesage

---
+++ src/components/ui/ToastMessage/ToastMessage.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { ToastMessage, ToastMessageprops } from "@components";
+
+describe('Topast Message', () => {
+    it("renders toast messages correctly", () => {
+        const toasts: ToastMessageprops[] = [
+          { id: 1, status: "success", message: "Success!", position: "top-right" },
+          { id: 2, status: "info", message: "Info!", position: "bottom-right" },
+          { id: 3, status: "error", message: "Error!", position: "top" },
+        ];
+    
+        render(<ToastMessage toasts={toasts} />);
+    
+        // Check that all messages are rendered
+        expect(screen.getByText("Success!")).toBeInTheDocument();
+        expect(screen.getByText("Info!")).toBeInTheDocument();
+        expect(screen.getByText("Error!")).toBeInTheDocument();
+      });
+})
